09b0b34d18b1e29ad8e722406b89187dc6ffa530
문일윤 <iym1511@naver.com>
2024-01-31 19:34:05 +0900

    MongoDB연결, MovieDB API 셋팅

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// api주소 상수
+export const API_URL = 'https://api.themoviedb.org/3/'
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
